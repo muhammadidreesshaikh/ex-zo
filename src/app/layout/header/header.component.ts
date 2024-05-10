eab44b82adf91a24949061b183633cc7789407a2
muhammadidreesshaikh <idreesiqbal104@gmail.com>
2024-05-11 00:32:09 +0500

    header responsive done

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  toggleButton: boolean = false;
 
+  toggleMenu() {
+    this.toggleButton = !this.toggleButton;
+  }
 }
